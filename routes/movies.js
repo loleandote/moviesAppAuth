7713f11474cc43aeee601576e141e9add33311e0
Alfonso Sebastián Simorte Ramos <117114417+loleandote22@users.noreply.github.com>
2024-10-31 23:48:23 +0100

    con formato

--- routes/movies.js
+++ routes/movies.js
@@ -13,36 +13,36 @@ var Movie = require("../models/Movie.js");
 
 mongoose.set("strictQuery", false);
 var db = mongoose.connection;
-function tokenVerify (req, res, next) {
-  var authHeader=req.get('authorization');
+function tokenVerify(req, res, next) {
+  var authHeader = req.get('authorization');
   const retrievedToken = authHeader.split(' ')[0];
-  
+
   if (!retrievedToken) {
-      res.status(401).send({
+    res.status(401).send({
+      ok: false,
+      message: "Token inválido"
+    })
+  } else {
+    jwt.verify(retrievedToken, process.env.TOKEN_SECRET, function (err, retrievedToken) {
+      if (err) {
+        res.status(401).send({
           ok: false,
           message: "Token inválido"
-      })
-  }else{       
-      jwt.verify(retrievedToken, process.env.TOKEN_SECRET,  function (err, retrievedToken) {
-          if (err) {
-              res.status(401).send({
-                  ok: false,
-                  message: "Token inválido"
-              });
-          } else {
-              next();
-          }
-      });
+        });
+      } else {
+        next();
+      }
+    });
   }
 }
 
-router.get("/secure", tokenVerify, 
+router.get("/secure", tokenVerify,
   function (req, res, next) {
-      debug("Acceso seguro con token a las películas");
-      Movie.find().sort("-creationdate").exec(function (err, movies) {
-          if (err) res.status(500).send(err);
-          else res.status(200).json(movies);
-      })
+    debug("Acceso seguro con token a las películas");
+    Movie.find().sort("-creationdate").exec(function (err, movies) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(movies);
+    })
   });
 /* GET movies listing */
 router.get("/", function (req, res) {
@@ -72,48 +72,39 @@ router.get("/:id", function (req, res) {
   });
 });
 
-  // /* POST a new movie*/
-  // router.post("/", function (req, res) {
-  //   Movie.create(req.body, function (err, movieinfo) {
-  //     if (err) res.status(500).send(err);
-  //     else res.sendStatus(200);
-  //   });
-  // });
+// /* POST a new movie*/
+// router.post("/", function (req, res) {
+//   Movie.create(req.body, function (err, movieinfo) {
+//     if (err) res.status(500).send(err);
+//     else res.sendStatus(200);
+//   });
+// });
 
-    /* POST a new movie (ahora conviene hacerlo todo con promesas) */
+/* POST a new movie (ahora conviene hacerlo todo con promesas) */
 router.post("/", crearPelicula);
-  // POST de una nueva pelicula
+// POST de una nueva pelicula
 router.post("/secure", tokenVerify, crearPelicula);
-function crearPelicula(req,res){
+function crearPelicula(req, res) {
   Movie.create(req.body).then(function (movie) {
     res.status(201).json(movie);
   }).catch(function (err) {
     res.status(500).send(err);
   });
 }
-function actualizaPelicula(req,res){
+router.put("/secure/:id", tokenVerify, function (req, res, next) {
+  debug("Modificación segura de una pelicula con token");
   Movie.findByIdAndUpdate(req.params.id, req.body, function (err, movieinfo) {
     if (err) res.status(500).send(err);
     else res.sendStatus(200);
-});
-}
-router.put("/:id", actualizaPelicula);
-router.put("/secure/:id", tokenVerify, function (req, res, next) {
-  debug("Modificación segura de una pelicula con token");
- actualizaPelicula(req,res);
-});
-router.delete("/:id", function (req, res, next) {
-  Movie.findByIdAndDelete(req.params.id, function (err, movieinfo) {
-      if (err) res.status(500).send(err);
-      else res.sendStatus(200);
-  });
-});
-router.delete("/secure/:id", tokenVerify, function (req, res, next) {
-  debug("Borrado seguro de una pelicula con token");
-  Movie.findByIdAndDelete(req.params.id, function (err, movieinfo) {
-      if (err) res.status(500).send(err);
-      else res.sendStatus(200);
   });
 });
 
-  module.exports = router;
+router.delete("/secure/:id", tokenVerify, function (req, res, next) {
+  debug("Borrado seguro de una pelicula con token");
+  Movie.findByIdAndDelete(req.params.id, function (err, movieinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+module.exports = router;
